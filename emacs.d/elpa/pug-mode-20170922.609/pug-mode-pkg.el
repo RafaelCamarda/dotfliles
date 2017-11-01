c20b4792f52348b2aa8e05e35d5b47a4c7f42667
Rafael Camarda <rafael.camarda@locaweb.com.br>
2017-11-01 13:36:10 -0200

    add emacs d

---
+++ emacs.d/elpa/pug-mode-20170922.609/pug-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pug-mode" "20170922.609" "Major mode for jade/pug template files" '((emacs "24.3")) :commit "7a1481b16b0f9e5d19eb48dc6d22d02aba1d57e0" :url "https://github.com/hlissner/emacs-pug-mode" :keywords '("markup" "language" "jade" "pug"))
---
+++ emacs.d/elpa/robe-20170611.1755/robe-autoloads.el
@@ -0,0 +1,59 @@
+;;; robe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ac-robe" "ac-robe.el" (22989 26377 803317
+;;;;;;  511000))
+;;; Generated autoloads from ac-robe.el
+
+(autoload 'ac-robe-available "ac-robe" "\
+Return t if `robe-mode' completions are available, otherwise nil.
+
+\(fn)" nil nil)
+
+(autoload 'ac-robe-setup "ac-robe" "\
+
+
+\(fn)" nil nil)
+
+(defconst ac-source-robe '((available . ac-robe-available) (prefix . ac-robe-prefix) (candidates . ac-robe-candidates) (document . ac-robe-doc) (symbol . "r")) "\
+`auto-complete' completion source for Ruby using `robe-mode'.")
+
+;;;***
+
+;;;### (autoloads nil "company-robe" "company-robe.el" (22989 26377
+;;;;;;  835317 581000))
+;;; Generated autoloads from company-robe.el
+
+(autoload 'company-robe "company-robe" "\
+A `company-mode' completion back-end for `robe-mode'.
+
+\(fn COMMAND &optional ARG &rest IGNORE)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "robe" "robe.el" (22989 26377 415316 672000))
+;;; Generated autoloads from robe.el
+
+(autoload 'robe-mode "robe" "\
+Improved navigation for Ruby.
+
+The following commands are available:
+
+\\{robe-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("robe-pkg.el") (22989 26377 379316 595000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; robe-autoloads.el ends here
